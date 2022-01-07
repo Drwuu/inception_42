b5fcde954258a7a02f0f9e1cfa479a3dc588843d
Ludwig WOURMS <lwourms@student.42lyon.fr>
2022-01-07 12:28:07 -0500

    docker-c.yml > added image name tag (if build used, just tag with name and doesnt dl image) | tool.sh > added --root to allow user creation)

--- srcs/requirements/wordpress/conf/tool.sh
+++ srcs/requirements/wordpress/conf/tool.sh
@@ -2,6 +2,6 @@
 wp core download --allow-root
 wp core config --dbname='wordpress' --dbuser='lwourms' --dbpass='superprout' --dbhost='mariadb:3306' --dbprefix='wp_' --allow-root
 wp core install --url="lwourms.42.fr" --title="lwourms inception_42 website" --admin_user="lwourms" --admin_password="superprout" --admin_email="lwourms@student.42lyon.fr" --allow-root
-wp user create toto toto@toto.com --role='subscriber' --user_pass='prout'
+wp user create toto toto@toto.com --role='subscriber' --user_pass='prout' --allow-root
 wp option update comment_registration 1 --allow-root
 exec /usr/sbin/php-fpm7.3 -F
